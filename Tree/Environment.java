0a700faabf53f024d57fb3356656e1e3503c0e17
jalenwhite6 <jwhi348@lsu.edu>
2026-04-13 05:35:24 +0000

    Enviornment.java change

--- Tree/Environment.java
+++ Tree/Environment.java
@@ -2,8 +2,8 @@ package Tree;
 
 public class Environment extends Node {
 
-    private Node scope;
-    private Environment env;
+    private Node scope;          // innermost frame (association list)
+    private Environment env;     // enclosing environment
 
     public Environment() {
         scope = Nil.getInstance();
@@ -15,40 +15,75 @@ public class Environment extends Node {
         env = e;
     }
 
+    public boolean isEnvironment() {
+        return true;
+    }
+
+    // =========================================================
+    // FIND helper: returns the binding pair (id . value-cell)
+    // =========================================================
     private static Node find(Node id, Node alist) {
         if (!alist.isPair()) return null;
 
         Node bind = alist.getCar();
-        if (id.getName().equals(bind.getCar().getName()))
+
+        if (id.getName().equals(bind.getCar().getName())) {
             return bind;
+        }
 
         return find(id, alist.getCdr());
     }
 
+    // =========================================================
+    // LOOKUP (variable access)
+    // =========================================================
     public Node lookup(Node id) {
         Node bind = find(id, scope);
 
-        if (bind != null) return bind.getCdr();
-        if (env != null) return env.lookup(id);
+        if (bind != null) {
+            // bind = (id value)
+            return bind.getCdr().getCar();   // ✅ FIXED
+        }
+
+        if (env != null) {
+            return env.lookup(id);
+        }
 
         throw new RuntimeException("Undefined variable: " + id.getName());
     }
 
+    // =========================================================
+    // DEFINE (add or update in current scope)
+    // =========================================================
     public void define(Node id, Node val) {
         Node bind = find(id, scope);
 
-        if (bind != null) bind.setCdr(val);
-        else scope = new Cons(new Cons(id, val), scope);
+        if (bind != null) {
+            bind.setCdr(new Cons(val, Nil.getInstance()));
+        } else {
+            scope = new Cons(
+                new Cons(id, new Cons(val, Nil.getInstance())),
+                scope
+            );
+        }
     }
 
+    // =========================================================
+    // ASSIGN (set!)
+    // =========================================================
     public void assign(Node id, Node val) {
         Node bind = find(id, scope);
 
-        if (bind != null) bind.setCdr(val);
-        else if (env != null) env.assign(id, val);
-        else throw new RuntimeException("Undefined variable: " + id.getName());
+        if (bind != null) {
+            bind.setCdr(new Cons(val, Nil.getInstance()));
+        } else if (env != null) {
+            env.assign(id, val);
+        } else {
+            throw new RuntimeException("Undefined variable: " + id.getName());
+        }
     }
 
+    // alias for Scheme set!
     public void set(Node id, Node val) {
         assign(id, val);
     }
