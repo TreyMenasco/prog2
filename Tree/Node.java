4f31e5712e701699451be59d4ef74d6f582c8caf
jalenwhite6 <jwhi348@lsu.edu>
2026-04-13 05:25:56 +0000

    changes again

--- Tree/Node.java
+++ Tree/Node.java
@@ -1,77 +1,24 @@
-// Node -- Super class for parse tree node objects
-
 package Tree;
 
 public class Node {
-    // The argument of print(int) is the number of characters to indent.
-    // Every subclass of Node must implement print(int).
+
     public void print(int n) {
     }
 
-    // The first argument of print(int, boolean) is the number of characters
-    // to indent. It is interpreted the same as for print(int).
-    // The second argument is only useful for lists (nodes of classes
-    // Cons or Nil). For all other subclasses of Node, the boolean
-    // argument is ignored. Therefore, print(n,p) defaults to print(n)
-    // for all classes other than Cons and Nil.
-    // For classes Cons and Nil, print(n,TRUE) means that the open
-    // parenthesis was printed already by the caller.
-    // Only classes Cons and Nil override print(int,boolean).
-    // For correctly indenting special forms, you might need to pass
-    // additional information to print. What additional information
-    // you pass is up to you. If you only need one more bit, you can
-    // encode that in the sign bit of n. If you need additional parameters,
-    // make sure that you define the method print in all the appropriate
-    // subclasses of Node as well.
     public void print(int n, boolean p) {
         print(n);
     }
 
-    // For parsing Cons nodes, for printing trees, and later for
-    // evaluating them, we need some helper functions that test
-    // the type of a node and that extract some information.
+    public boolean isBoolean() { return false; }
+    public boolean isNumber() { return false; }
+    public boolean isString() { return false; }
+    public boolean isSymbol() { return false; }
+    public boolean isNull() { return false; }
+    public boolean isNil() { return this == Nil.getInstance(); }
+    public boolean isPair() { return false; }
+    public boolean isProcedure() { return false; }
+    public boolean isEnvironment() { return false; }
 
-    // These are implemented in the appropriate subclasses to return true.
-    public boolean isBoolean() {
-        return false;
-    }
-
-    public boolean isNumber() {
-        return false;
-    }
-
-    public boolean isString() {
-        return false;
-    }
-
-    public boolean isSymbol() {
-        return false;
-    }
-
-    public boolean isNull() {
-        return false;
-    }
-    public boolean isNil() {
-        return this == Nil.getInstance();
-    }
-    public boolean isPair() {
-        return false;
-    }
-
-    public boolean isProcedure() {
-        return false;
-    }
-
-    public boolean isEnvironment() {
-        return false;
-    }
-
-    // Report an error in these default methods and implement them
-    // in class Cons. After setCar, a Cons cell needs to be `parsed' again
-    // using parseList.
-
-    // These static methods were added to allow the Node hierarchy to
-    // be modified without having to recompile classes Parser and Printer.
     public static void print(Node t, int n, boolean p) {
         t.print(n, p);
     }
@@ -114,10 +61,129 @@ public class Node {
         return "";
     }
 
+    // ===================== EVAL =====================
     public Node eval(Environment env) {
-        System.err.println("Error: Node.eval not yet implemented");
-        return Nil.getInstance();
+
+        // Self-evaluating
+        if (this.isNumber() || this.isString() || this.isBoolean()) {
+            return this;
+        }
+
+        // Variable lookup
+        if (this.isSymbol()) {
+            return env.lookup(this);
+        }
+
+        // Must be a list
+        if (!this.isPair()) {
+            System.err.println("Error: invalid expression");
+            return Nil.getInstance();
+        }
+
+        Node first = this.getCar();
+        Node rest = this.getCdr();
+
+        // -------- Special Forms --------
+        if (first.isSymbol()) {
+            String name = first.getName();
+
+            // quote
+            if (name.equals("quote")) {
+                return rest.getCar();
+            }
+
+            // if
+            else if (name.equals("if")) {
+                Node cond = rest.getCar().eval(env);
+
+                if (!(cond instanceof BooleanLit && cond == BooleanLit.getInstance(false))) {
+                    return rest.getCdr().getCar().eval(env);
+                } else {
+                    return rest.getCdr().getCdr().getCar().eval(env);
+                }
+            }
+
+            // define
+            else if (name.equals("define")) {
+                Node var = rest.getCar();
+                Node val = rest.getCdr().getCar().eval(env);
+
+                env.define(var, val);
+                return var;
+            }
+
+            // lambda
+            else if (name.equals("lambda")) {
+                return new Closure(this, env);
+            }
+
+            // begin
+            else if (name.equals("begin")) {
+                Node result = Nil.getInstance();
+                Node current = rest;
+
+                while (!current.isNil()) {
+                    result = current.getCar().eval(env);
+                    current = current.getCdr();
+                }
+                return result;
+            }
+
+            // set!
+            else if (name.equals("set!")) {
+                Node var = rest.getCar();
+                Node val = rest.getCdr().getCar().eval(env);
+
+                env.set(var, val);
+                return val;
+            }
+
+            // cond
+            else if (name.equals("cond")) {
+                Node clauses = rest;
+
+                while (!clauses.isNil()) {
+                    Node clause = clauses.getCar();
+
+                    Node test = clause.getCar();
+                    Node expr = clause.getCdr().getCar();
+
+                    if (test.isSymbol() && test.getName().equals("else")) {
+                        return expr.eval(env);
+                    }
+
+                    Node testVal = test.eval(env);
+
+                    if (!(testVal instanceof BooleanLit && testVal == BooleanLit.getInstance(false))) {
+                        return expr.eval(env);
+                    }
+
+                    clauses = clauses.getCdr();
+                }
+
+                return Nil.getInstance();
+            }
+        }
+
+        // -------- Function Call --------
+        Node func = first.eval(env);
+        Node args = evalList(rest, env);
+
+        return func.apply(args);
     }
+
+    // ===================== EVAL LIST =====================
+    private Node evalList(Node list, Environment env) {
+        if (list.isNil()) {
+            return Nil.getInstance();
+        }
+
+        return new Cons(
+            list.getCar().eval(env),
+            evalList(list.getCdr(), env)
+        );
+    }
+
     public Node apply(Node args) {
         System.err.println("Error: trying to apply a non-function");
         return Nil.getInstance();
